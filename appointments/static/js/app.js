67a451225ea332e27123bf4a04dded07ae3ca01f
ido sarue <dodojoker67@gmail.com>
2021-09-08 22:20:59 +0300

    addedenv

---
+++ appointments/static/js/app.js
@@ -0,0 +1,19 @@
+
+$(function date () {
+    $("#datepicker").datepicker({
+        changeMonth: true,
+        dateFormat: "dd-mm-yy",
+        beforeShowDay: function(d) {
+        var day = d.getDay();
+        return [(day != 5 && day != 6)];
+    }
+    });
+});
+
+$(function birth_picker() {
+    $("#datepicker_birth").datepicker({
+        changeYear: true,
+        yearRange: "1930:2003",
+        dateFormat: "dd-mm-yy",
+    })
+});
