c81367af5bf765c5308ba32e89871b0d419d3669
ido sarue <dodojoker67@gmail.com>
2021-10-07 20:00:44 +0300

    changed settings

--- appointments/appointments/settings.py
+++ appointments/appointments/settings.py
@@ -21,9 +21,10 @@ from django.utils.translation import gettext_lazy as _
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
