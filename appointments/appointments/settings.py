b46496fa190866f526adb6d520f88f3b1ed98b5d
ido sarue <dodojoker67@gmail.com>
2021-10-07 19:44:13 +0300

    changed settings

--- appointments/appointments/settings.py
+++ appointments/appointments/settings.py
@@ -175,7 +175,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'testdjangosaru@gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
