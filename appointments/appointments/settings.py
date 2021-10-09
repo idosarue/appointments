412141067f828379512f165f8cfe24166a4d2794
ido sarue <dodojoker67@gmail.com>
2021-10-09 11:59:34 +0300

    changed settings

--- appointments/appointments/settings.py
+++ appointments/appointments/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-ALLOWED_HOSTS = ['appointmentsd', 'herokuapp']
+ALLOWED_HOSTS = ['https://appointmentsd.herokuapp.com/', 'localhost']
 
 # Application definition
 
