301e8a8d4ccadc616af47d92f699572de8185370
ido sarue <dodojoker67@gmail.com>
2021-10-09 20:40:32 +0300

    changed models

--- appointments/therapist/models.py
+++ appointments/therapist/models.py
@@ -33,8 +33,8 @@ class Date(models.Model):
 
 
 class WorkingTime(models.Model):
-    start_time = models.TimeField(default="08:30")
-    end_time = models.TimeField(default="16:30")
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     break_time = models.IntegerField(default=15)
 
 
